5a7e874e778a9e39a1f4615061827eef61598be2
Fabian Nguyen <Fabian.Nguyen@united-it.net>
2019-08-26 22:33:38 +0200

    Fixed pickle load

--- migration.py
+++ migration.py
@@ -1,6 +1,8 @@
 import pickle
 import redis
-data = pickle.loads('migrate')
+infile = open('migrate','rb')
+data = pickle.load(infile)
+infile.close()
 r = redis.from_url(os.environ.get("REDIS_URL"))
 pdb = pickle.dumps(data)
 r.set(user_id,pdb)

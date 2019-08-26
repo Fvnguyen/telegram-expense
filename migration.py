d95d54afdbad86082b65054bacddd5e823879764
Fabian Nguyen <Fabian.Nguyen@united-it.net>
2019-08-26 22:45:39 +0200

    Migration was done, obsolete file was set inactive

--- migration.py
+++ migration.py
@@ -1,9 +1,9 @@
-import pickle
-import redis
-import os
-infile = open('migrate','rb')
-data = pickle.load(infile)
-infile.close()
-r = redis.from_url(os.environ.get("REDIS_URL"))
-pdb = pickle.dumps(data)
-r.set('961108390',pdb)
+# import pickle
+# import redis
+# import os
+# infile = open('migrate','rb')
+# data = pickle.load(infile)
+# infile.close()
+# r = redis.from_url(os.environ.get("REDIS_URL"))
+# pdb = pickle.dumps(data)
+# r.set('961108390',pdb)

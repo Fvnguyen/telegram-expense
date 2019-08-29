ffb0b98429161f2640f528f5d16dd3cf50ea2815
Fabian Nguyen <Fabian.Nguyen@united-it.net>
2019-08-29 12:22:30 +0200

    Added last_5 as picture

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ import redis
 from datetime import datetime
 import pandas as pd
 from plotnine import *
+import numpy as np
+import matplotlib.pyplot as plt
+import six
 
 LIST_OF_ADMINS = [961108390]
 TELEGRAM_TOKEN = "936719065:AAEtWah8YV4x_68CFxXkOeJGvbsk5KukyrI"
@@ -56,6 +59,29 @@ def loadDF(userID):
         df = False
         print('No DF')
         return df
+
+def render_mpl_table(data, col_width=3.0, row_height=0.625, font_size=14,
+                     header_color='#40466e', row_colors=['#f1f1f2', 'w'], edge_color='w',
+                     bbox=[0, 0, 1, 1], header_columns=0,
+                     ax=None, **kwargs):
+    if ax is None:
+        size = (np.array(data.shape[::-1]) + np.array([0, 1])) * np.array([col_width, row_height])
+        fig, ax = plt.subplots(figsize=size)
+        ax.axis('off')
+
+    mpl_table = ax.table(cellText=data.values, bbox=bbox, colLabels=data.columns, **kwargs)
+
+    mpl_table.auto_set_font_size(False)
+    mpl_table.set_fontsize(font_size)
+
+    for k, cell in  six.iteritems(mpl_table._cells):
+        cell.set_edgecolor(edge_color)
+        if k[0] == 0 or k[1] < header_columns:
+            cell.set_text_props(weight='bold', color='w')
+            cell.set_facecolor(header_color)
+        else:
+            cell.set_facecolor(row_colors[k[0]%len(row_colors) ])
+    return ax
 
 # usage restriction
 
@@ -331,8 +357,11 @@ def show_last(update, context):
     df = loadDF(user_id)
     try:
         df['Zeit'] = df['Zeit'].dt.strftime('%d/%m/%y')
-        last_5 = df.tail(5).iloc[:,1:4].to_string(index = False,col_space = 9)
-        context.bot.send_message(chat_id=update.message.chat_id, text=last_5)
+        #last_5 = df.tail(5).iloc[:,1:4].to_string(index = False,col_space = 9)
+        last_5 = df.tail(5).iloc[:,1:4]
+        last_5 = render_mpl_table(last_5, header_columns=0, col_width=4)
+        last_5.get_figure().savefig('last_5.png')
+        context.bot.send_photo(chat_id=update.message.chat_id, photo=open('last_5.png', 'rb'))
         return ConversationHandler.END
     except:
         context.bot.send_message(chat_id=update.message.chat_id, text="Noch keine Daten.")

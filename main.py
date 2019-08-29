82ed99789956589b7d8ce930772a6fb71274308b
Fabian Nguyen <Fabian.Nguyen@united-it.net>
2019-08-29 12:31:14 +0200

    Reduced bbox

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ def loadDF(userID):
 
 def render_mpl_table(data, col_width=3.0, row_height=0.625, font_size=14,
                      header_color='#40466e', row_colors=['#f1f1f2', 'w'], edge_color='w',
-                     bbox=[0, 0, 1, 1], header_columns=0,
+                     bbox=[0, 0, 0, 0], header_columns=0,
                      ax=None, **kwargs):
     if ax is None:
         size = (np.array(data.shape[::-1]) + np.array([0, 1])) * np.array([col_width, row_height])
